4786b4e8859fe98ca286ae92b6df43c66b435348
aurinko <aurinko@naomi-gw>
2019-10-14 00:56:19 +0300

    cake -> hfsc + fq_codel

--- tc_qdisc.sh
+++ tc_qdisc.sh
@@ -1,12 +1,26 @@
 #!/bin/bash
 
-up_speed=9900
-down_speed=9900
-lan_iface=enp8s0f1
-wan_iface=enp8s0f0
+wan_iface="enp8s0f0"
+lan_iface="enp8s0f1"
+up=9900
+down=9900
+tc=/sbin/tc
+egress="fq_codel noecn flows 8192 target 5ms"
+ingress="fq_codel noecn flows 8192 target 5ms"
 
-tc qdisc del dev ${lan_iface} root
-tc qdisc del dev ${wan_iface} root
+echo Deleting old qdiscs ...
+tc qdisc del dev ${wan} root
+tc qdisc del dev ${laniverkko} root
 
-tc qdisc add dev ${lan_iface} root cake bandwidth ${down_speed}Mbit ethernet ether-vlan internet triple-isolate no-split-gso
-tc qdisc add dev ${wan_iface} root cake bandwidth ${up_speed}Mbit ethernet ether-vlan internet triple-isolate no-split-gso
+echo Adding new qdiscs ...
+
+echo EGRESS ..
+$tc qdisc add dev ${wan} root handle 1:0 hfsc default 1
+$tc class add dev ${wan} parent 1:0 classid 1:1 hfsc ls rate ${up}mbit ul rate ${up}mbit
+$tc qdisc add dev ${wan} parent 1:1 handle 2: ${egress}
+
+echo INGRESS ..
+$tc qdisc add dev ${laniverkko} root handle 1:0 hfsc default 1
+$tc class add dev ${laniverkko} parent 1:0 classid 1:1 hfsc ls rate ${down}mbit ul rate ${down}mbit
+$tc qdisc add dev ${laniverkko} parent 1:1 handle 2: ${ingress}
+
